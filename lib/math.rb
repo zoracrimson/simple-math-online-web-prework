8654a9c0c0dd3e23ff7784d51783c1a08bef6304
arelenglish <arelenglish@gmail.com>
2014-11-06 14:43:00 -0500

    added solution

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+  num1 + num2
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 end
 
 def division(num1, num2)
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  + (( * ) / )
+  num1 + ((num2 * num3) / num4)
 end
