dfec18b5424b43bcbf417e87dda370970359c97c
Kat Maldon <katharinamaldon@gmail.com>
2018-11-15 19:52:41 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,25 @@
 def addition(num1, num2)
+  num1 + num2
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 end
 
 def division(num1, num2)
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
